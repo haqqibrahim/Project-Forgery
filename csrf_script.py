95de152b3a1209ab741c44e4afe0eb8876c0dce7
haqqibrahim <enessyibrahim@gmail.com>
2023-10-04 09:41:57 +0100

    Init

---
+++ csrf_script.py
@@ -0,0 +1,65 @@
+# exploit_scripts/csrf_exploit_generator.py
+import os
+import requests
+import sys
+import logging
+from jinja2 import Template
+from utils.colors import colors
+
+class CSRFTester:
+    def __init__(self, target_url, parameters, values, http_method='POST'):
+        self.target_url = target_url
+        self.parameters = parameters
+        self.values = values
+        self.http_method = http_method
+        self.cookies = dict()
+
+    def generate_exploit_html(self):
+        try:
+            template_filename = (
+                "templates/post_exploit_template.html"
+                if self.http_method == 'POST'
+                else "templates/get_exploit_template.html"
+            )
+
+            with open(template_filename, "r") as template_file:
+                template_content = template_file.read()
+
+            template = Template(template_content)
+            
+            parameters_values_pairs = list(zip(self.parameters, self.values))
+
+            exploit_html = template.render(target_url=self.target_url, parameters_values_pairs=parameters_values_pairs)
+
+            return exploit_html
+        except Exception as e:
+            logging.error(f"An error occurred: {str(e)}")
+            return None
+
+if __name__ == "__main__":
+    logging.basicConfig(filename="csrf_test.log", level=logging.INFO)
+
+    try:
+        print(colors.fg.purple + "===================PROJECT FORGERY V1.0.0==================" + colors.reset)
+        print(colors.fg.purple + "==============AUTOMATED CSRF PoC SCRIPT GENERATOR==========" + colors.reset)
+        print(colors.fg.purple + "\tCreator By : Haqq the Bug Hunter" + colors.reset)
+
+        target_url = input("Enter the target URL (e.g., https://example.com): ")
+        parameters = input("Enter the parameter names separated by commas: ").split(',')
+        values = input("Enter the corresponding values separated by commas: ").split(',')
+        http_method = input("Enter HTTP method (POST/GET, default: POST): ").upper() or 'POST'
+
+        csrf_tester = CSRFTester(target_url, parameters, values, http_method)
+
+        exploit_html = csrf_tester.generate_exploit_html()
+
+        if exploit_html:
+            output_filename = "csrf_exploit.html"
+            with open(output_filename, "w") as exploit_file:
+                exploit_file.write(exploit_html)
+            print(colors.fg.green + f"CSRF exploit HTML file '{output_filename}' created successfully." + colors.reset)
+        else:
+            print(colors.fg.red + "Failed to generate the CSRF exploit HTML." + colors.reset)
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+        print(colors.fg.red + "An error occurred. Check the log file for details." + colors.reset)
---
+++ utils/colors.py
@@ -0,0 +1,16 @@
+class colors:
+    reset = '\033[0m'
+    class fg:
+        red = '\033[31m'
+        green = '\033[32m'
+        orange = '\033[33m'
+        blue = '\033[34m'
+        purple = '\033[35m'
+        cyan = '\033[36m'
+        lightgrey = '\033[37m'
+        darkgrey = '\033[90m'
+        lightred = '\033[91m'
+        lightgreen = '\033[92m'
+        yellow = '\033[93m'
+        lightblue = '\033[94m'
+        pink = '\033[95m'
